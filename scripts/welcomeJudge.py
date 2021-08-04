44e82c3b6681354cb434bfc0ce32c64affb94b21
PuReum Choi <poo1994@hanmail.net>
2021-08-04 19:27:22 +0900

    Create welcomeJudge.py

---
+++ scripts/welcomeJudge.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+print("[Welcome PR!]")
+print("Please regist comment with Command")
+print("run : run (path)/문제 테스팅(Success/Error)")
+print("    ex) run test/Implementation/2857/main.java")
