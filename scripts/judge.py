ddbdbc3bb819b1d8ad087e068e9999e349096087
PuReum Choi <poo1994@hanyang.ac.kr>
2021-08-04 16:42:45 +0900

    Update judge.py

--- scripts/judge.py
+++ scripts/judge.py
@@ -51,7 +51,7 @@ result   = html.select('body > div.wrapper > div.container.content > div > secti
 memory   = html.select('body > div.wrapper > div.container.content > div > section > div:nth-child(3) > div > table > tbody > tr > td:nth-child(2)')[0].text + " KB"
 time     = html.select('body > div.wrapper > div.container.content > div > section > div:nth-child(3) > div > table > tbody > tr > td:nth-child(3)')[0].text + " ms"
 
-if result != "맞았습니다!!":
+if result != "[Success!]":
     check_all = False
 
 if not check_all:
