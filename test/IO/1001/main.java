2e05a00a8281e8b8e2c6a62abe0b814c4672f633
PuReum Choi <poo1994@hanmail.net>
2021-08-04 17:48:53 +0900

    Create main.java

---
+++ test/IO/1001/main.java
@@ -0,0 +1,15 @@
+// Authored by : choipureum
+// Co-authored by : -
+// Link : http://boj.kr/5b9d4795090c489c9702c20cbefe8517
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		System.out.println(A-B);
+	
+}
+}
