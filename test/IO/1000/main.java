5470033d7febc73460ab339365c455a0fc457127
PuReum Choi <poo1994@hanmail.net>
2021-08-04 16:32:51 +0900

    Update main.java

--- test/IO/1000/main.java
+++ test/IO/1000/main.java
@@ -1,52 +1,15 @@
 // Authored by : choipureum
 // Co-authored by : -
-// Link : http://boj.kr/4ea0c0604cd04373a901ec2223db6b99
+// Link : http://boj.kr/a7d9860598fe4927bb773cea7d3e4218
 
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        FastReader rd = new FastReader();
-
-        int a = rd.nextInt();
-        int b = rd.nextInt();
-        System.out.println(a + b);
-    }
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        String next() {
-            while(st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() { return Integer.parseInt(next()); }
-        long nextLong() { return Long.parseLong(next()); }
-        double nextDouble() { return Double.parseDouble(next()); }
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-    }
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		int A=scan.nextInt();
+		int B=scan.nextInt();
+		System.out.println(A+B);
+	
+}
 }
