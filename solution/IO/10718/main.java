a75505064816935fc7d7955cc1eba94e9d84c6e5
SANGOK <tkddhr12345@naver.com>
2021-08-05 22:37:37 +0900

    Add [10718] : java solution(We love kriii)

---
+++ solution/IO/10718/main.java
@@ -0,0 +1,10 @@
+// Authored by : ChoiSangok
+// Co-authored by : -
+// Link : http://boj.kr/bd21933bb3a44232b4f216a5c6e91428
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+}
